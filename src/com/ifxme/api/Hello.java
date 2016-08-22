8718858351b28231510ebcffc5c932acfbb783d9
teng1234567ha@126.com <slbw2012>
2016-08-22 17:36:46 +0800

    完善请求的方式

--- src/com/ifxme/api/Hello.java
+++ src/com/ifxme/api/Hello.java
@@ -1,8 +1,6 @@
 package com.ifxme.api;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -16,12 +14,14 @@ public class Hello {
     public String sayPlainTextHello() {
         return "Hello Jersey";
     }
+
     // This method is called if XML is request
     @GET
     @Produces(MediaType.TEXT_XML)
     public String sayXMLHello() {
         return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
     }
+
     // This method is called if HTML is request
     @GET
     @Produces(MediaType.TEXT_HTML)
@@ -35,5 +35,17 @@ public class Hello {
     @Produces(MediaType.APPLICATION_JSON)
     public String sayJsonHello() {
         return "{\"code\":0,\"data\":{\"country\":\"\\u4e2d\\u56fd\",\"country_id\":\"CN\",\"area\":\"\\u534e\\u4e1c\",\"area_id\":\"300000\",\"region\":\"\\u6c5f\\u82cf\\u7701\",\"region_id\":\"320000\",\"city\":\"\\u5357\\u4eac\\u5e02\",\"city_id\":\"320100\",\"county\":\"\",\"county_id\":\"-1\",\"isp\":\"\\u7535\\u4fe1\",\"isp_id\":\"100017\",\"ip\":\"114.221.128.140\"}}";
+    }
+
+    @GET
+    @Path("/{id}/info")
+    public String deleteContact(@PathParam("id") String id) {
+        return "info," + id;
+    }
+
+    @GET
+    @Path("/user")
+    public String testParam(@QueryParam("userId") String userId) {
+        return "hello," + userId;
     }
 }
