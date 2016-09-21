aeedddd4fcd158b309e20727bfe33ff4d18bfab8
teng1234567ha@126.com <slbw2012>
2016-09-21 16:12:16 +0800

    修改task实体类，和部分task的业务操作

--- src/com/ifxme/api/Response.java
+++ src/com/ifxme/api/Response.java
@@ -6,12 +6,21 @@ import javax.xml.bind.annotation.XmlRootElement;
  * Created by abcca on 2016/8/22 0022.
  */
 public class Response {
+    /**
+     * 成功的code
+     */
+    public static final int CODE_SUCCESS = 1;
     private int code;
     private Object data;
+    private String msg;
 
     public Response(int code, Object data) {
         this.code = code;
-        this.data = data;
+        if (code == CODE_SUCCESS) {
+            this.data = data;
+        } else {
+            msg = (String) data;
+        }
     }
 
     public int getCode() {
@@ -28,5 +37,13 @@ public class Response {
 
     public void setData(Object data) {
         this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
     }
 }
